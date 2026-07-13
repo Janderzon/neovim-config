b464e1d8567ea95ce5a61c5873f253cc18bc66fa
James Anderson <james.anderson2@btinternet.com>
2026-07-13 22:16:26 +0100

    Add csharp treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "rust", "toml", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+      ensure_installed = { "rust", "toml", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "c_sharp" },
       highlight = { enable = true },
       indent = { enable = true },
     })
