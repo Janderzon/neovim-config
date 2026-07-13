6cdea3d3aa968cd834f712b78f8530f69406176d
James Anderson <james.anderson2@btinternet.com>
2026-07-13 22:27:23 +0100

    Add go to definition

--- init.lua
+++ init.lua
@@ -47,6 +47,11 @@ vim.keymap.set("n", "<leader>ca", function()
   vim.cmd.RustLsp("codeAction")
 end, { desc = "Rust code action" })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight yanked text",
