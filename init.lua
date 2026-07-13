ca58ef99b07bdccb56a653701de6f773b49e30e6
James Anderson <james.anderson2@btinternet.com>
2026-07-13 21:50:48 +0100

    Highlight on yank

--- init.lua
+++ init.lua
@@ -48,6 +48,13 @@ vim.keymap.set("n", "<leader>ca", function()
 end, { desc = "Rust code action" })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight yanked text",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
+
 require("lazy").setup("plugins", {
     rocks = {
         enabled = false,
